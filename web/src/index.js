d124fc3dba745e5dab2e831e84be3053d4da045f
Pedro Landim <henriquep35036@gmail.com>
2021-07-29 17:10:53 -0300

    Ajuste inicial do projeto

---
+++ web/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Cadastro from './pages/cadastro';
+import './styles/global.css';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Cadastro />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
