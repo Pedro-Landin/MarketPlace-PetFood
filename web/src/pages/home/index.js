195f917d087282fa47957f26c7302c0210e57e99
Pedro Landim <henriquep35036@gmail.com>
2021-08-05 18:12:37 -0300

    Inicio da pagina home

---
+++ web/src/pages/home/index.js
@@ -0,0 +1,12 @@
+import './styles.css'
+import Header from '../../components/header';
+
+const Home = () => {
+    return (
+        <div className="h-100">
+            <Header/>
+        </div>
+    )
+}
+
+export default Home;
